987cff1c55f504840fe64b2976505948c942ae22
xiaoxiaoxu <2371169686@qq.com>
2019-06-29 21:35:24 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// 1.引入element-ui
+import ElementUI from "element-ui";
+// 2.引入css
+import 'element-ui/lib/theme-chalk/index.css';
+
+import "./assets/css/index.css";
+
+// 3.安装插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+
---
+++ src/views/Login.vue
@@ -0,0 +1,119 @@
+<template>
+  <!-- el-row 这个组件就相当于是一个div，这个组件是用来做布局的 -->
+  <!-- type 是布局模式，可以是flex -->
+  <!-- justify 是用来设置水平对齐的方式的 -->
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <!-- el-col 布局组件 列组件 -->
+    <!-- span 属性是用来设置当前列所占的格数的 -->
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form
+        ref="loginForm"
+        :rules="formRules"
+        :model="form"
+        class="login-form"
+        label-position="top"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+// 要实现表单校验：
+// 1. 先给数据中添加一个校验规则对象
+// 2. rules: {表单绑定的数据的名称: [{required: true, message: "提示信息", trigger: "触发校验的时机blur change"}]}
+// 3. 需要把这个校验规则对象绑定到el-form组件上 :rules="校验规则则对象"
+// 4. 需要给每一项被校验的el-form-item组件添加 prop属性 属性值就是 绑定的数据的名称
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      formRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          },
+          { 
+            min: 5, 
+            max: 15, 
+            message: "用户名长度在 5 到 15 个字符", 
+            trigger: "change" 
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          { 
+            min: 6, 
+            max: 10, 
+            message: "密码长度在 6 到 10 个字符", 
+            trigger: "change" 
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+
+      this.$refs[formName].validate(valid => {
+
+        if (valid) {
+          
+          axios({
+            url : "http://localhost:8888/api/private/v1/login",
+            method : "post",
+            data : this.form
+          }).then(({data : {data,meta}}) =>{
+            if(meta.status == 200){
+              // localStorage.setItem("token", data.token);
+              this.$router.push("/home")
+            }
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style>
+.row-bg {
+  height: 100%;
+  background-color: #2d434c;
+}
+.login-form {
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px 20px;
+  min-width: 400px;
+}
+</style>
+
