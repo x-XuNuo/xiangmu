822b5c294c9d8b09f0691e4b599801ec8ddb733f
xiaoxiaoxu <2371169686@qq.com>
2019-06-30 09:15:30 +0800

    登录首页导航

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from "./views/Home.vue"
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path : "/login",
@@ -14,6 +14,33 @@ export default new Router({
     {
       path : "/home",
       component : Home
+    },
+    {
+      path : "/",
+      redirect : "/home"
     }
   ]
 });
+
+
+// 注册一个导航守卫
+router.beforeEach((to, from, next) => {
+  
+  // 1.判断用户访问的是不是登录页面,如果是登录页面，直接放行
+  if (to.path === "/login") {
+    
+    next();
+    return;
+  }
+  // 2. 如果访问的是其他的页面，判断用户是否进行了登录
+  //    2.1 如果登录了，那就直接放行
+  //    2.2 如果没登录，那就跳转到登录页
+
+  if (localStorage.getItem("token")) {
+    next();
+  } else {
+    router.push("/login")
+  }
+});
+
+export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>
-    1111
-  </div>
+  <el-container>
+
+    <el-header>
+      <el-row type="flex"  justify="space-between">
+        <el-col :span="6">
+          <img src="../assets/logo.png" alt="">
+        </el-col>
+        <el-col>
+          <h1>品优购后台管理系统</h1>
+        </el-col>
+        <el-col :span="6" class="header-right">
+          <div>
+            欢迎39期会员
+            <a href="##">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px">
+        Aside            
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+
+<script>
+export default {};
+</script>
+
+<style>
+.el-container {
+  height: 100%;
+}
+.el-container .el-header {
+  line-height: 60px;
+  background-color: #b3c1cd;
+  padding: 0;
+}
+.el-container .el-header img{
+  width: 200px;
+}
+.el-container .el-header h1{
+  font-size: 28px;
+  color: #fff;
+  font-weight: bolder;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+.header-right{
+  font-weight: bolder;
+  text-align: right;
+  padding-right: 20px;
+}
+.el-container .el-header div a {
+    color: orange;
+}
+</style>
+
