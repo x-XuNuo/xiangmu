26997f313f1eb7b2aa1218687e6b0e57b3f82d41
xiaoxiaoxu <2371169686@qq.com>
2019-07-04 17:21:23 +0800

    权限列表完成

--- src/views/Rights/Rights.vue
+++ src/views/Rights/Rights.vue
@@ -6,22 +6,66 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 数据 -->
+    <el-table
+      ref="singleTable"
+      :data="rightsList"
+      highlight-current-row
+      style="width: 100%"
+    >
+      <el-table-column type="index" width="50">
+
+      </el-table-column>
+      <el-table-column property="authName" label="权限名称" width="120"></el-table-column>
+      <el-table-column property="path" label="路径" width="120"></el-table-column>
+      <el-table-column label="层级">
+        <template v-slot="{row}">
+          <span>{{row.level | levelfilter}}</span>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  
-}
+  filters : {
+    levelfilter(value){
+      switch(value){
+        case "0":
+          return "一级";
+        case "1":
+          return "二级";
+        case "2":
+          return "三级";
+
+      }
+    }
+  },
+    data() {
+        return {
+            rightsList: []
+        };
+    },
+  async created() {
+    let res = await this.$http({
+      url: "rights/list"
+    });
+
+    console.log(res)
+
+    this.rightsList = res.data.data;
+  }
+};
 </script>
 
 <style>
-  .el-breadcrumb.el-breadcrumb {
-    height: 50px;
-    line-height: 50px;
-    padding-left: 10px;
-    font-size: 16px;
-    background-color: cyan;
-
-  }
+.el-breadcrumb.el-breadcrumb {
+  height: 50px;
+  line-height: 50px;
+  padding-left: 10px;
+  font-size: 16px;
+  background-color: cyan;
+}
 </style>
